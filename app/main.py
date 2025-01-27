e8b0b501d506d7f884a2c5edd8b9529f428c3118
DmytroPavlichko <160277367+DmytroPavlichko@users.noreply.github.com>
2025-01-27 18:56:48 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -27,30 +27,7 @@ async def lifespan(app: FastAPI):
     # await bot.delete_webhook()
     # await stop_bot()
     # logging.info("Webhook deleted")
-"""
 
-# app = FastAPI()
-
-
-@app.on_event("startup")
-async def on_startup():
-    logging.info("Starting bot setup...")
-    dp.include_router(bot_router)
-    await start_bot()
-    webhook_url = settings.get_webhook_url()
-    await bot.set_webhook(url=webhook_url,
-                          allowed_updates=dp.resolve_used_update_types(),
-                          drop_pending_updates=True)
-    logging.info(f"Webhook set to {webhook_url}")
-
-
-@app.on_event("shutdown")
-async def on_shutdown():
-    logging.info("Shutting down bot...")
-    await bot.delete_webhook()
-    await stop_bot()
-    logging.info("Webhook deleted")
-"""
 
 app = FastAPI(lifespan=lifespan)
 
